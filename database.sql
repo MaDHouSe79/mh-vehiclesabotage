7d641cac84f91f296b305ca5d46bbc9f7a94bb6d
MaDHouSe <madhouse1979@gmail.com>
2024-10-25 01:41:33 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `mh_brakes` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `plate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `wheel_lf` int(10) NOT NULL DEFAULT 0,
+    `wheel_rf` int(10) NOT NULL DEFAULT 0,
+    `wheel_lr` int(10) NOT NULL DEFAULT 0,
+    `wheel_rr` int(10) NOT NULL DEFAULT 0,
+    `oil_empty` int(10) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC; 
